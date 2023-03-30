f4715d939574af803b189738166e24be691a4417
Kaity <kate@Kaitlyns-MBP.lan>
2023-03-30 16:07:20 -0500

    typos fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express ();
 const db = mongoose.connection;
 // const cors = require('cors');
 require('dotenv').config();
-const Cities = require('./models/weather.js')
+const Weather = require('./models/weather.js')
 
 
 //Port
@@ -13,7 +13,12 @@ const PORT = process.env.PORT
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to Mongo &
-mongoose.connect(MONGODB_URI, console.log('MongoDB listening...'))
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => {
+  console.log('MongoDB listening...')
+});
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,8 +28,8 @@ app.get('/' , (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/cities', (req, res) => {
-  Cities.find({}).then((foundWeather) => {
+app.get('/weather', (req, res) => {
+  Weather.find({}).then((foundWeather) => {
     res.json(foundWeather)
   })
 });

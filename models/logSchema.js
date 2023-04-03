ed99e07faa371bf479e131d3ce21e4048f60dc22
Kaity <kate@Kaitlyns-MBP.lan>
2023-04-03 14:08:54 -0500

    update

--- models/logSchema.js
+++ models/logSchema.js
@@ -3,35 +3,35 @@ const mongoose = require('mongoose')
 const logSchema = new mongoose.Schema ({
   city: {
     type: String,
-    required: true
+
   },
   country: {
     type: String,
-    required: true
+   
   },
   description: {
     type: String,
-    required: true
+
   },
   travelers: {
     type: String,
-    required: false
+
   },
   startDate: {
     type: Date,
-    required: true
+ 
   },
   endDate: {
     type: Date,
-    required: true
+
   },
   rating: {
     type: Number,
-    required: true
+
   }, 
   image: {
     type: String,
-    required: true
+ 
   }
 
 })
